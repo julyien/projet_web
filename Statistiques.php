9f1319a9d0eae2d4c8a42ab7ba4bd2e4462dcc39
keysha18 <nicolas.journel@gmail.com>
2022-03-25 11:45:05 +0100

    update Nico

---
+++ Statistiques.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="Statistiques.css" rel="Stylesheet" type="text/css"/>
+</head>
+<body>
+    <div class="topnav">
+        <a class="active"><img src="logo.png" width="45px"/></a>
+        <a>
+            <input type="text" name="text" class="search" placeholder="Recherchez ici!">
+            <input type="submit" name="submit" class="submit" value="Search">
+        </a>
+        <a href="#Acceuil">Accueil</a>
+        <a href="#Stages">Stages</a>
+        <a href="#Souhaits">Souhaits</a>
+        <a href="#Notifications">Notifications</a>
+        <a href="#Messagerie">Messagerie</a>
+        <a href="#Profil">Profil</a>
+    </div>
+    <br>
+    <div class="center" > <h2> Statistiques : </h2> </div>
+    <br>
+    <br>
+    <br>
+    <br>
+
+<div class="row">
+    <div class="c col-md-3">
+        <div class="brd">
+            <pre>Nom entreprise</pre>
+            <pre>Description de l'entreprise</pre>
+            <pre>Evaluation des utilisateurs</pre>
+            <div class="c col-md-7 mx-auto">
+                <div class="brd">
+                Nombre d'étudiants ayant postulé chez cette entreprise :
+                </div>
+            </div>
+</div>
+
+</div>
+
+
+<div class="c col-md-5 offset-md-1">
+    <div class="brd">
+        <div class="a"><button type="button">Favoris</button></div>
+            <pre>Titre du poste</pre>
+            <pre>Description du poste</pre>
+        </div>
+            <div class="b">
+                <br>
+                Evaluer l'entreprise : <button type="button">Like</button>   <button type="button">Dislike</button> </div>
+                <div class="a">
+                <br>
+                <button type="button">Détails</button>       <button type="button">Postuler</button> </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br><br><br>
+<div class="c col-md-5 offset-md-4">
+    <div class="brd">
+        <pre>Statistiques du poste :</pre>
+        <br><br>
+        <pre>Compétences</pre>
+        <pre>Localités</pre>
+        <pre>Entreprise</pre>
+        <pre>Type de promotion concernées</pre>
+        <pre>Durée du stage</pre>
+        <pre>Base de rémunération</pre>
+        <pre>Date de l'offre</pre>
+        <pre>Nombre de place offertes aux étudiants</pre>
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+    
+</body>
+</html>
---
+++ stage_etudiant.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="stage_etudiant.css" rel="Stylesheet" type="text/css"/>
+</head>
+<body>
+    <div class="topnav">
+        <a class="active"><img src="logo.png" width="45px"/></a>
+        <a>
+            <input type="text" name="text" class="search" placeholder="Recherchez ici!">
+            <input type="submit" name="submit" class="submit" value="Search">
+        </a>
+        <a href="#Acceuil">Accueil</a>
+        <a href="#Stages">Stages</a>
+        <a href="#Souhaits">Souhaits</a>
+        <a href="#Notifications">Notifications</a>
+        <a href="#Messagerie">Messagerie</a>
+        <a href="#Profil">Profil</a>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <div class="c col-6 mx-auto"  >
+        <div class="brd">
+            <div class="a"><button type="button">Favoris</button></div>
+            <pre>Offre de Stages :</pre>
+            <pre>Nom entreprise</pre>
+            <pre>Nom du poste</pre>
+            <pre>Localisation</pre>
+            <div class="a">
+                <button type="button">Postuler</button> <button type="button">En savoir plus</button>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="c col-6 mx-auto">
+        <div class="brd">
+            <div class="a"><button type="button">Favoris</button></div>
+            <pre>Offre de Stages :</pre>
+            <pre>Nom entreprise</pre>
+            <pre>Nom du poste</pre>
+            <pre>Localisation</pre>
+            <div class="a">
+                <button type="button">Postuler</button> <button type="button">En savoir plus</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
