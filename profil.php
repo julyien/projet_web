68d7b95d72a5ab45c61ef22622d0e38f2de49013
Coeurdecedre <vincent.leleu@viacesi.fr>
2022-03-28 14:15:06 +0200

    maj profil

--- profil.php
+++ profil.php
@@ -21,7 +21,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="profil.css" rel="Stylesheet" type="text/css"/>
+    <link href="projet.css" rel="Stylesheet" type="text/css"/>
     <script src="https://kit.fontawesome.com/d37d50e548.js"></script>
     <link rel="manifest" href="manifest.json">
     <script>
@@ -36,24 +36,11 @@
 <?php
 
 
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "projetweb";
-  
-  $conn = mysqli_connect($server,$username,$password,$dbname);
+
 
             
         $sql = "SELECT * FROM profil";
-        $result = mysqli_query($conn,$sql);
-        $queryResults = mysqli_num_rows($result);
 
-        if ($queryResults > 0){
-            while ($row = mysqli_fetch_assoc($result)){
-                echo "<div class='profil-box'>
-                </div>";
-            }
-        }
  
 
 ?>
