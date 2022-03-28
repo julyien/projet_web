298016cb7a663e2e7fe8b975b0166b45bd01e80e
Coeurdecedre <vincent.leleu@viacesi.fr>
2022-03-28 10:59:58 +0200

    modif barre de recherche acceuil

--- acceuil.php
+++ acceuil.php
@@ -1,33 +1,3 @@
-<?php
-
-session_start();
-$dbh = new PDO('mysql:host=localhost;dbname=projetweb', 'root', '');
-
-
-if ( !empty($_POST)) {
-   
-
-$Pseudo= $_POST['identifiant_profil'];
-$MDP= $_POST['password_profil'];
-$x=0;
-
-
-
-foreach($dbh->query('SELECT * from profil') as $row){
-
-    if ($Pseudo == $row['identifiant_profil'] && $MDP == $row['password_profil']){
-        $_SESSION['identifiant_profil'] = $Pseudo['identifiant_profil'];
-         header('location:acceuil.php');
-         exit;
-    }
-
-    else{
-        echo 'Identifiant ou mot de passe incorrect.';
-    }
-        
-}
-}
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -47,12 +17,36 @@ foreach($dbh->query('SELECT * from profil') as $row){
       </script>
 </head>
 <body>
+<?php
+
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "projetweb";
+  
+  $conn = mysqli_connect($server,$username,$password,$dbname);
+
+            
+        $sql = "SELECT * FROM profil";
+        $result = mysqli_query($conn,$sql);
+        $queryResults = mysqli_num_rows($result);
+
+        if ($queryResults > 0){
+            while ($row = mysqli_fetch_assoc($result)){
+                echo "<div class='profil-box'>
+                </div>";
+            }
+        }
+ 
+
+?>
     <div class="topnav">
-        <a><img src="logo.png" width="45px"/></a>
-        <a>
-            <input type="text" name="text" class="search" placeholder="Recherchez ici!">
-            <input type="submit" name="submit" class="submit" value="Search">
-        </a>
+        <a class="active"><img src="logo.png" width="45px"/></a>
+        <form action="search.php" method="POST">
+            <input type="text" name="identifiant_profil" class="search" placeholder="Recherchez ici!" value="<?php if(isset($Search)){ echo $Search; }?>">
+            <input type="submit" name="submit-search" class="submit" value="Search">
+        </form>
         <a href="#Acceuil">Accueil</a>
         <a href="#Stages">Stages</a>
         <a href="ListeDeSouhait.php">Souhaits</a>
