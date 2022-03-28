f90a84a8b7637233519a8d5cd3bc9e756721de73
julyien <julienhedin@gmail.com>
2022-03-28 15:03:10 +0200

    update vincent degueu

--- acceuil.php
+++ acceuil.php
@@ -2,58 +2,97 @@
   session_start(); 
   include('connexionDB.php'); 
 
-    $getid = intval($_SESSION['id_profil']);
-
-
-    $req = $DB->query("SELECT * FROM profil INNER JOIN centre_formation ON profil.id_centre = centre_formation.id_centre WHERE id_profil = ? ", array($getid));
-    $row = $req->fetch();
-
-
-
+  if (isset($_SESSION['id_profil'])) {
+        $getid = intval($_SESSION['id_profil']);
+        
+        $req = $DB->query("SELECT * FROM profil INNER JOIN centre_formation ON profil.id_centre = centre_formation.id_centre WHERE id_profil = ? ", array($getid));
+        $row = $req->fetch();
+    }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="projet.css" rel="Stylesheet" type="text/css"/>
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="projet.css" rel="stylesheet" type="text/css">
     <link rel="manifest" href="manifest.json">
     <script>
         //if browser support service worker
-        if('serviceWorker' in navigator) {
-          navigator.serviceWorker.register('sw.js');
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('sw.js');
         };
-      </script>
+    </script>
 </head>
+
 <body>
-<?php  
-        $sql = "SELECT * FROM profil";
-?>
-    <div class="topnav">
-        <a class="active"><img src="logo.png" width="45px"/></a>
-        <form action="search.php" method="POST">
-            <input type="text" name="identifiant_profil" class="search" placeholder="Recherchez ici!" value="<?php if(isset($Search)){ echo $Search; }?>">
-            <input type="submit" name="submit-search" class="submit" value="Search">
-        </form>
-        <div class="profil-container">
+    <nav class="navbar navbar-expand navbar-dark topnav">
+        <div class="container-fluid">
+            <a class="active"><img src="logo.png" width="45"></a>
+            <div class="collapse navbar-collapse ms-2">
+                <form action="search.php" method="POST">
+                    <input class="form-control" type="text" name="identifiant_profil" placeholder="Search" aria-label="Search">
+                </form>
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#Acceuil">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Stages">Stages</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Souhaits">Souhaits</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Gestion">Gestion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Notifications">Notifications</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Profil">Profil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logout.php">Se déconnecter</a>
+                    </li>
+                    
+                </ul>
+            </div>
         </div>
-        <a href="#Acceuil">Accueil</a>
-        <a href="#Stages">Stages</a>
-        <a href="ListeDeSouhait.php">Souhaits</a>
-        <a href="Gestion_entreprise.php">Gestion</a>
-        <a href="notification.php">Notifications</a>
-        <a href="#Messagerie">Messagerie</a>
-        <a href="profil.php">Profil</a>
-        <div class=a>
-        <a href="logout.php">Se deconnecter</a>
-    </div>
-    </div>
+    </nav>
     <div class="d">
-        <h1>Bienvenue <?php echo $row['prenom_profil']; ?> </h1>
+        <h6>Bienvenue <?php echo $row['prenom_profil']; ?></h6>
+    </div>
+    <div class="c col-6 mx-auto">
+        <div class="brd">
+            <div class="a"><button type="button">Favoris</button></div>
+            <pre>Offre de Stages :</pre>
+            <pre>Nom entreprise</pre>
+            <pre>Nom du poste</pre>
+            <pre>Localisation</pre>
+            <div class="a">
+                <button type="button">Postuler</button> <button type="button">En savoir plus</button>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="c col-6 mx-auto">
+        <div class="brd">
+            <div class="a"><button type="button">Favoris</button></div>
+            <pre>Offre de Stages :</pre>
+            <pre>Nom entreprise</pre>
+            <pre>Nom du poste</pre>
+            <pre>Localisation</pre>
+            <div class="a">
+                <button type="button">Postuler</button> <button type="button">En savoir plus</button>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
