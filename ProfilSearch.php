1f77edde0b931bd085cd184d47e8f829d047fa0b
keysha18 <nicolas.journel@gmail.com>
2022-03-31 21:08:48 +0200

    ProfilSearch avec la bonne forme + Validator

--- ProfilSearch.php
+++ ProfilSearch.php
@@ -68,7 +68,8 @@
     </nav>
     <br>
     <br>
-    <div class="d col-4">
+    <div class="brd">
+    <div class="d col-12">
     <i class="fa-solid fa-circle-user"></i><?php echo $row['prenom_profil']; ?> !
     <br>
     <br>
@@ -84,6 +85,7 @@
             <pre><i class="fa-solid fa-file-pdf"></i><input type="file" accept="application/pdf,application/vnd.ms-excel">  Ajouter une lettre de motivation</pre>
         </div>
     </div>
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
