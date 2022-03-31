8a7564e08da0e2af8b98865b9c13f4f608f14e92
Coeurdecedre <vincent.leleu@viacesi.fr>
2022-03-31 15:36:33 +0200

    maj addwish

--- AddWishlist.php
+++ AddWishlist.php
@@ -7,6 +7,6 @@ $getidprofil = intval($_SESSION['id_profil']);
 $getidoffre = intval($_SESSION['id_offre']);
 
 $req = $DB->query("INSERT INTO wishlist (id_offre, id_profil) values($getidoffre, $getidprofil)");
-header('Location:');
+header('Location: stage_etudiant.php');
 
 ?>
