6adf20340bd6484463bc6ca6a00b7723060e3bdb
keysha18 <nicolas.journel@gmail.com>
2022-03-31 21:15:01 +0200

    search.php = clean

--- search.php
+++ search.php
@@ -9,10 +9,14 @@ $conn = mysqli_connect($server,$username,$password,$dbname);
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recherche</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="projet.css" rel="stylesheet" type="text/css">
@@ -30,7 +34,7 @@ $conn = mysqli_connect($server,$username,$password,$dbname);
 
 <br> 
 
-<h1> <div class="d"> Page de recherche </div> </h1>
+<div class="d"> <h1>Page de recherche</h1> </div>
 
 <br>
 <br>
