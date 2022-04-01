15eccfc0e494da56c3f75f226aef5a0bb7b9c298
Coeurdecedre <vincent.leleu@viacesi.fr>
2022-04-01 09:35:51 +0200

    maj adwish

--- AddWishlist.php
+++ AddWishlist.php
@@ -1,6 +1,11 @@
 <?php                                                                                                //Ce script permet d'ajouter une offre a la liste de souhait
 session_start();
 
+if(!isset($_SESSION['id_profil'])){ 
+    header('Location: Authentification.php'); 
+    exit; 
+  }
+
 include('connexionDB.php'); 
 
 $getidprofil = intval($_SESSION['id_profil']);
